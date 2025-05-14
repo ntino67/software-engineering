53fd3f48b81a1fb78a5f02d7bff951a7aa9fc0bd
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-14 14:33:46 +0200

    Question 1 part 2

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main()
     {
-        Process.Start("explorer.exe");
-        Console.WriteLine("Explorer launched");
+        Process process = new Process();
+        process.StartInfo.FileName = "notepad.exe";
+        process.Start();
+
+        Console.WriteLine("Notepad launched");
     }
 }
